483f80949495090760f6b91691c4a9dd57331c44
Eric Chang <mc@mc4.us>
2021-06-28 12:10:19 +0800

    update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,10 +1,15 @@
 <?php
-define('URL', 'https://share.abc.com/');
+//Sets the URL that will be shown to users for each upload. Must be set and must have tailing slash
+define('URL', 'https://share.ematters.com.tw/');
 define('TITLE', '#PictShare#');
+//If set, will only show the upload form and allow to upload via API if request is coming from this subnet
 define('ALLOWED_SUBNET', '192.168.11.0/24');
+//If set, will whitelist content controllers for your instance. Must be uppercase and can be comma separated. Example: Only Pictures: IMAGE, Pictures and Videos: IMAGE,VIDEO
 define('CONTENTCONTROLLERS', '');
 define('MASTER_DELETE_CODE', '');
 define('MASTER_DELETE_IP', '');
+//If set, all uploads require this code via GET or POST variable "uploadcode" or upload will fail
+// not implemented
 define('UPLOAD_FORM_LOCATION', '');
 define('UPLOAD_CODE', '');
 define('LOG_UPLOADER', false);
